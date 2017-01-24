33849b36e0733df165b35df7935c3953e448799c
Gavin Scheele <gdscheele@gmail.com>
2017-01-24 10:50:55 -0800

    Gavin - added position saving to get speed

--- sensors/get_speed.py
+++ sensors/get_speed.py
@@ -6,6 +6,7 @@ import constants
 
 def getSpeed(ser1, ser_str, wheel_circumference, dist_brake, pod_data, accData, sql_wrapper, logging, q):
     wheel_dist = 0
+    braked = False
     while True:
         # logging.debug("Getting speed from "+ser_str)
         bytesToRead = ser1.inWaiting()
@@ -19,28 +20,9 @@ def getSpeed(ser1, ser_str, wheel_circumference, dist_brake, pod_data, accData, 
             logging.debug("bytes to read was " + str(bytesToRead))
             wheel_dist += wheel_circumference
             logging.debug(ser_str + " dist is now " + str(wheel_dist)) 
-            sql_wrapper.execute("INSERT INTO " + ser_str + "speed VALUES (%s,%s)", (datetime.datetime.now().strftime(constants.TIME_FORMAT), response))
-            # try:
-            #     db1.execute("INSERT INTO " + ser_str + "speed VALUES (%s,%s)", (datetime.datetime.now(), response))
-            #     conn.commit()
-            # except Exception as e:
-            #
-            #     logging.debug("error writing to db")
-            #     logging.debug("e is " + str(type(e)))
-            #     print e
-            #     conn.rollback()
-                # if we have gone the distance
-        if wheel_dist > dist_brake:
+            sql_wrapper.execute("INSERT INTO " + ser_str + "speed VALUES (%s,%s,%d)", (datetime.datetime.now().strftime(constants.TIME_FORMAT), response, wheel_dist))
+
+        if wheel_dist > dist_brake and not braked:
             # push state checks to make sure that we've been going long enough that we're not still being pushed
             q.put("brake")
-            # if the acc detects we aren't being pushed
-            # if accData.y_g > -0.1:
-            #     logging.debug("Passed the distance on " + ser_str + ".  Lets brake")
-            #     logging.debug("Y G's detected as " + str(accData.y_g))
-            #     q.put("brake")
-            # else:
-            #     logging.debug("Passed the braking distance but Y G's was " + str(
-            #         accData.y_g) + " so we are still accelerating probably")
-
-
-                # return 1
+            braked = True

9a5666c1be4bb2eac3b31bf371e70a7f04ca6760
Mitchell Artin <mitchella@Test-Tablet.local>
2017-01-28 13:29:06 -0500

    Mitch - added calced_acc

--- states/brake_2_state.py
+++ states/brake_2_state.py
@@ -13,9 +13,9 @@ def start(pod_data, drive_controller, sql_wrapper):
             logging.debug("POD STOPPED")
             break
         measured_velocity = max(pod_data.wheel_1_speed, pod_data.wheel_2_speed)
-        #calculated_acceleration = \
-        #    (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
-        calculated_acceleration = 50
+        calculated_acceleration = \
+            (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
+
         logging.debug("calced acc " + str(calculated_acceleration))
         if calculated_acceleration > 1.3 * constants.GRAVITY:
             logging.debug("brakes are not stopping fast enough. pulse brakes")

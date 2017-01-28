20593398365960eddd9fc1c978ed4050454ba61f
Mitchell Artin <mitchella@Test-Tablet.local>
2017-01-28 00:21:02 -0500

    Mitch - random fixes from the real world test

--- states/brake_2_state.py
+++ states/brake_2_state.py
@@ -13,9 +13,10 @@ def start(pod_data, drive_controller, sql_wrapper):
             logging.debug("POD STOPPED")
             break
         measured_velocity = max(pod_data.wheel_1_speed, pod_data.wheel_2_speed)
-        calculated_acceleration = \
-            (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
-
+        #calculated_acceleration = \
+        #    (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
+        calculated_acceleration = 50
+        logging.debug("calced acc " + str(calculated_acceleration))
         if calculated_acceleration > 1.3 * constants.GRAVITY:
             logging.debug("brakes are not stopping fast enough. pulse brakes")
             drive_controller.send_pulse_main_brakes()
@@ -26,3 +27,4 @@ def start(pod_data, drive_controller, sql_wrapper):
         time.sleep(.1)
         previous_velocity_sample_time = datetime.datetime.now()
         previous_velocity = max(pod_data.wheel_1_speed, pod_data.wheel_2_speed)
+        time.sleep(.3)

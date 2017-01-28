341d2b3561eb06b79414c7185c13ad1577503e95
Gavin Scheele <gdscheele@gmail.com>
2017-01-27 17:12:21 -0800

    Gavin - new braking algorithm

--- states/brake_2_state.py
+++ states/brake_2_state.py
@@ -5,12 +5,8 @@ import datetime
 
 
 def start(pod_data, drive_controller, sql_wrapper):
-    # 20% of total expected brake time - .1 seconds
-    time.sleep(1.81)
     previous_velocity_sample_time = datetime.datetime.now()
     previous_velocity = max(pod_data.wheel_1_speed, pod_data.wheel_2_speed)
-    # finish 20% of braking time
-    time.sleep(.1)
 
     while True:
         if pod_data.stopped:
@@ -18,22 +14,12 @@ def start(pod_data, drive_controller, sql_wrapper):
             break
         measured_velocity = max(pod_data.wheel_1_speed, pod_data.wheel_2_speed)
         calculated_acceleration = \
-             (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
+            (measured_velocity - previous_velocity)/(datetime.datetime.now() - previous_velocity_sample_time).total_seconds()
 
-        # max is closest to the end of the tube
-        measured_distance = max(pod_data.wheel_1_dist, pod_data.wheel_2_dist)
-        calculated_stopped_distance = \
-            measured_distance + ((-1 * measured_velocity) ** 2)/(2 * calculated_acceleration * constants.GRAVITY)
-        if calculated_stopped_distance > constants.STOPPED_DISTANCE:
-            logging.debug("brakes are not stopping fast enough. activate the second one")
-            drive_controller.send_engage_auxililary_brakes()
-            while drive_controller.get_response() != constants.ENGAGE_AUXILIARY_BRAKES + "\n":
-                drive_controller.send_engage_auxiliary_brakes()
-                time.sleep(.1)
-            sql_wrapper.execute("""INSERT INTO states VALUES (NULL,%s,%s)""",
-                                (datetime.datetime.now().strftime(constants.TIME_FORMAT), "AUX BRAKE ENGAGED"))
-            logging.debug("second brake activated")
-            break
+        if calculated_acceleration > 1.3 * constants.GRAVITY:
+            logging.debug("brakes are not stopping fast enough. pulse brakes")
+            drive_controller.send_pulse_main_brakes()
+            drive_controller.send_pulse_aux_brakes()
         else:
             logging.debug("brakes are stopping fine, pod still going")
 
